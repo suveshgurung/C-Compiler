b1de366bbb8f86b813125d6e1a550b15ea5d0fd4
suvesh <suveshgurung2@gmail.com>
2024-09-06 22:27:34 +0545

    token structure made

--- main.c
+++ main.c
@@ -3,7 +3,14 @@
 #include <stddef.h>
 #include <sys/types.h>
 #include <string.h>
+// #include <ctype.h>
 #include <stdio.h>
+
+typedef enum {
+    KEYWORD,
+    STRING,
+    SPECIAL_CHARACTER
+} Token_List;
 
 /* structure to store the lines read */
 typedef struct {
@@ -11,9 +18,17 @@ typedef struct {
     size_t len;
 } Read_Characters;
 
+/* structure which stores the tokens */
+typedef struct {
+    Token_List tokenType;   
+    char *value;
+} Token;
+
+
 #define INIT_RC_BUF { NULL, 0 }
 
 void appendLine(Read_Characters *buf, const char *str, ssize_t length);
+void tokenize(const char *str, Token *tokenBuf);
 
 int main(int argc, char *argv[]) {
     
@@ -32,16 +47,19 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    Token tokenBuf;
     char *line = NULL;
     size_t length;
     ssize_t numOfCharRead;
 
     while ((numOfCharRead = getline(&line, &length, fp)) != -1) {
+        tokenize(line, &tokenBuf);
         appendLine(&charBuf, line, numOfCharRead);
     }
 
-    printf("%s", charBuf.line);
+    // printf("%s", charBuf.line);
 
+    free(charBuf.line);
     free(line);
     fclose(fp);
 
@@ -50,9 +68,8 @@ int main(int argc, char *argv[]) {
 
 
 /* functions definition*/
-
 void appendLine(Read_Characters *buf, const char *str, ssize_t length) {
-    char *temp = (char *)realloc(buf->line, (buf->len + (size_t)length) + 1);
+    char *temp = (char *)realloc(buf->line, (buf->len + (size_t)length + 1));
 
     if (temp == NULL) {
         perror("realloc");
@@ -65,4 +82,14 @@ void appendLine(Read_Characters *buf, const char *str, ssize_t length) {
     buf->len += length;
 
     buf->line[buf->len] = '\0';
+}
+
+void tokenize(const char *str, Token *tokenBuf) {
+    size_t i = 0;
+    char c;
+    while ((c = str[i]) != '\n') {
+        printf("%c", c);
+        ++i;
+    }
+    printf("\n");
 }
